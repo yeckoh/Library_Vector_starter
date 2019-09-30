45a602e86bd57c34f0df970b6d7c5b3648cbde06
yeckoh <yeckoh@gmail.com>
2019-09-30 08:32:34 -0700

    loadBooks line parser ready

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,12 +1,42 @@
 #include "../includes_usr/fileIO.h"
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
 using namespace std;
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries in books
  * 			SUCCESS if all data is loaded
  * */
-int loadBooks(std::vector<book> &books, const char* filename)
+int loadBooks(vector<book> &books, const char* filename)
 {
+	ifstream fin(filename, fstream::in);
+	if(!fin.is_open())
+		return COULD_NOT_OPEN_FILE;
+	if(fin.peek() == ifstream::traits_type::eof())
+		return NO_BOOKS_IN_LIBRARY;
+	book book_loader;
+	string line = "";
+	string token = "";
+	while(getline(fin, line)) {
+		stringstream line_stream(line);
+		//while(getline(line_stream, token, ',')) {
+		getline(line_stream, token, ',');
+			book_loader.book_id = stoi(token.substr(token.find_first_not_of(' ')));
+			getline(line_stream, token, ',');
+			book_loader.title = token.substr(token.find_first_not_of("\" "), token.find_last_of('\"')-1);
+			getline(line_stream, token, ',');
+			book_loader.author = token.substr(token.find_first_not_of("\" "), token.find_last_of('\"')-1);
+			getline(line_stream, token, ',');
+			book_loader.state = book_checkout_state(stoi(token.substr(token.find_first_not_of(' '))));
+			getline(line_stream, token, ',');
+			book_loader.loaned_to_patron_id = stoi(token.substr(token.find_first_not_of(' ')));
+//		}
+		cout << "loaded: " << book_loader.book_id << " " << book_loader.title << " " << book_loader.author << " " << book_loader.state << " " << book_loader.loaned_to_patron_id << "\n";
+	}
+	//cout << loader.book_id << loader.title << loader.author << loader.state << loader.loaned_to_patron_id;
 	return SUCCESS;
 }
 
@@ -15,7 +45,7 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries books (do not create file)
  * 			SUCCESS if all data is saved
  * */
-int saveBooks(std::vector<book> &books, const char* filename)
+int saveBooks(vector<book> &books, const char* filename)
 {
 	return SUCCESS;
 }
@@ -25,7 +55,7 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * 			NO_PATRONS_IN_LIBRARY if there are 0 entries in patrons
  * 			SUCCESS if all data is loaded
  * */
-int loadPatrons(std::vector<patron> &patrons, const char* filename)
+int loadPatrons(vector<patron> &patrons, const char* filename)
 {
 	return SUCCESS;
 }
@@ -35,7 +65,7 @@ int loadPatrons(std::vector<patron> &patrons, const char* filename)
  * 			NO_PATRONS_IN_LIBRARY if there are 0 entries in patrons  (do not create file)
  * 			SUCCESS if all data is saved
  * */
-int savePatrons(std::vector<patron> &patrons, const char* filename)
+int savePatrons(vector<patron> &patrons, const char* filename)
 {
 	return SUCCESS;
 }
